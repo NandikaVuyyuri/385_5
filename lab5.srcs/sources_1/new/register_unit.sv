68454bec8e5836808925dcade419b3081b59f796
akerans <akerans2@illinois.edu>
2024-10-09 15:58:23 -0500

    register unit finished

--- lab5.srcs/sources_1/new/register_unit.sv
+++ lab5.srcs/sources_1/new/register_unit.sv
@@ -17,26 +17,15 @@ module register_unit(
  );
  
  logic [7:0] demux;
- logic [7:0] demux_sr1;
- logic [7:0] demux_sr2;
+ logic [15:0] out0, out1, out2, out3, out4, out5, out6, out7;
 
  // dr -> 0,1,2,3,4,5,6,7 whatever bit is high corresponds to whatever register should be loaded
  demux_reg demuxer_gen(
     .demux_in   (dr),
     .demux_out  (demux)
  );
- //get registers for sr1 and sr2
-  demux_reg demuxer_sr1(
-    .demux_in   (sr1_in),
-    .demux_out  (demux_sr1)
- );
-  demux_reg demuxer_sr2(
-    .demux_in   (sr2_in),
-    .demux_out  (demux_sr2)
- );
 
- //WIP - HOW TO OUTPUT 2 REGS?? ALONG WITH ONLY 1 REGISTER LOGIC?
- //that way dr controls which register gets loaded with d_in
+
 load_reg #(.DATA_WIDTH(16)) reg0 (
     .clk    (clk),
     .reset  (reset),
@@ -44,22 +33,155 @@ load_reg #(.DATA_WIDTH(16)) reg0 (
     .load   (demux[0]), //each demux signal says which reg to load
     .data_i (d_in),     //load register with data in
 
-    .data_q (sr1_out)   //when only using 1 reg, it outputs to sr1
+    .data_q (out0)   //outputs are kept as variables inside reg unit
+                     //then assigned to sr1 or sr2 depending on sr_in
 );
 
 load_reg #(.DATA_WIDTH(16)) reg1 (
     .clk    (clk),
     .reset  (reset),
 
-    .load   (demux[1]), //each demux signal says which reg to load
-    .data_i (d_in),     //load register with data in
+    .load   (demux[1]),
+    .data_i (d_in),
 
-    .data_q (sr1_out)   //when only using 1 reg, it outputs to sr1
+    .data_q (out1)
 );
-//copy 8 times for 8 registers...
-//figure out the sr1 and sr2 conlfict thing FIRST !!!!
-  
+
+load_reg #(.DATA_WIDTH(16)) reg2 (
+    .clk    (clk),
+    .reset  (reset),
+
+    .load   (demux[2]),
+    .data_i (d_in),
+
+    .data_q (out2)
+);
+
+load_reg #(.DATA_WIDTH(16)) reg3 (
+    .clk    (clk),
+    .reset  (reset),
+
+    .load   (demux[3]),
+    .data_i (d_in),
+
+    .data_q (out3)
+);
+
+load_reg #(.DATA_WIDTH(16)) reg4 (
+    .clk    (clk),
+    .reset  (reset),
+
+    .load   (demux[4]),
+    .data_i (d_in),
+
+    .data_q (out4)
+);
+
+load_reg #(.DATA_WIDTH(16)) reg5 (
+    .clk    (clk),
+    .reset  (reset),
+
+    .load   (demux[5]),
+    .data_i (d_in),
+
+    .data_q (out5)
+);
+
+load_reg #(.DATA_WIDTH(16)) reg6 (
+    .clk    (clk),
+    .reset  (reset),
+
+    .load   (demux[6]),
+    .data_i (d_in),
+
+    .data_q (out6)
+);
+
+load_reg #(.DATA_WIDTH(16)) reg7 (
+    .clk    (clk),
+    .reset  (reset),
+
+    .load   (demux[7]),
+    .data_i (d_in),
+
+    .data_q (out7)
+);
+
+//determine what register output gets put into sr1_out and sr2_out
+always_comb
+begin  
+    case(sr1_in)
+        3'b000:
+        begin
+            sr1_out = out0;
+        end
+        3'b001:
+        begin
+            sr1_out = out1;
+        end
+        3'b010:
+        begin
+            sr1_out = out2;
+        end
+        3'b011:
+        begin
+            sr1_out = out3;
+        end
+        3'b100:
+        begin
+            sr1_out = out4;
+        end
+        3'b101:
+        begin
+            sr1_out = out5;
+        end
+        3'b110:
+        begin
+            sr1_out = out6;
+        end
+        3'b111:
+        begin
+            sr1_out = out7;
+        end
+    endcase
+    
+    case(sr2_in)
+        3'b000:
+        begin
+            sr2_out = out0;
+        end
+        3'b001:
+        begin
+            sr2_out = out1;
+        end
+        3'b010:
+        begin
+            sr2_out = out2;
+        end
+        3'b011:
+        begin
+            sr2_out = out3;
+        end
+        3'b100:
+        begin
+            sr2_out = out4;
+        end
+        3'b101:
+        begin
+            sr2_out = out5;
+        end
+        3'b110:
+        begin
+            sr2_out = out6;
+        end
+        3'b111:
+        begin
+            sr2_out = out7;
+        end
+    endcase
+end
 endmodule
+
 
 module demux_reg(
     input logic [2:0] demux_in,
@@ -105,4 +227,5 @@ begin
         end
     endcase
 end
+
 endmodule
